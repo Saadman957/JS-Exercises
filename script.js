de1f47a199a37f9580132c830a88675a2d14657b
Alim Al Saadman <saadmanal21@gmail.com>
2018-05-21 03:02:19 +0000

    task2

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+//task 1
 console.log("external JS");
+
+//task 2
+
+function sumArray(arr) {
+  var sum=0;
+        arr.forEach(function(value, index){
+          sum+=value;
+
+        });
+        return sum;
+        }
+
+        console.log(sumArray([1,2,3,4,5]));
+        console.log(sumArray([19,-2,30,-45]));
