142f5a048d962be39bfe3c6bcb1b0bc231314a74
Alim Al Saadman <saadmanal21@gmail.com>
2018-05-21 03:18:35 +0000

    task3

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ function sumArray(arr) {
 
         console.log(sumArray([1,2,3,4,5]));
         console.log(sumArray([19,-2,30,-45]));
+
+//Task 3
+
+function checkEmail(emailString){
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  var match = emailFormat.test(emailString);
+  return match;
+}
+
+console.log(checkEmail("john@smith.com"));
+console.log(checkEmail("johnsmith.info"));
