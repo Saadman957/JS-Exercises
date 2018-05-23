4c482a6ea9fa6f082ca14e917536d7c84afd8bc8
Alim Al Saadman <saadmanal21@gmail.com>
2018-05-23 02:56:28 +0000

    5a

--- script.js
+++ script.js
@@ -50,3 +50,30 @@ function getReadingStatus(index){
 }
 
 console.log(getReadingStatus(2));
+
+//Task 5
+
+var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+//5a
+   function addItem(){
+    cart.push(newItem);
+    console.log(cart);
+   }
+var newItem={name:"Watch", price: 64, quantity: 1}
+    addItem(newItem);
+
